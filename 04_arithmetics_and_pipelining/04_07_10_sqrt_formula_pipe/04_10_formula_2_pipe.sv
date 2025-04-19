77b9e5c926e84b3a16b7a09d3b683a145e1c7b75
Andrey Kazachkov <an.kazachkov3234@gmail.com>
2025-04-19 23:49:39 +0300

    04_07_10 fixed

--- 04_arithmetics_and_pipelining/04_07_10_sqrt_formula_pipe/04_10_formula_2_pipe.sv
+++ 04_arithmetics_and_pipelining/04_07_10_sqrt_formula_pipe/04_10_formula_2_pipe.sv
@@ -42,38 +42,62 @@ module formula_2_pipe
     // FPGA-Systems Magazine :: FSM :: Issue ALFA (state_0)
     // You can download this issue from https://fpga-systems.ru/fsm#state_0
 
-    logic vld1, vld2, vld3;
-    logic [31:0] res1, res2, res3;
-
-    isqrt sqrt1(
-          .clk(clk),
-          .rst(rst),
-          .x_vld(arg_vld),
-          .x(a),
-          .y_vld(vld1),
-          .y(res1)
-    );
-
-    isqrt sqrt2(
-          .clk(clk),
-          .rst(rst),
-          .x_vld(vld1 & arg_vld),
-          .x(res1 + b),
-          .y_vld(vld2),
-          .y(res2)
-    );
-
-    isqrt sqrt3(
-          .clk(clk),
-          .rst(rst),
-          .x_vld(vld2 & arg_vld),
-          .x(res2 + c),
-          .y_vld(vld3),
-          .y(res3)
-    );    
-
-    assign res_vld = vld3;
-    assign res = res3;
-
+     logic        sqrt_c_vld;
+     logic [15:0] sqrt_c_res;
+     
+     logic        sqrt_bc_vld;
+     logic [15:0] sqrt_bc_res;
+     
+     logic [31:0] a_reg [0:7]; 
+     logic [31:0] b_reg [0:3]; 
+     
+     always_ff @(posedge clk) begin
+         if (rst) begin
+             for (int i = 0; i < 8; i++) 
+                 a_reg[i] <= 0;
+             for (int i = 0; i < 4; i++) 
+                 b_reg[i] <= 0;
+         end else begin
+             if (arg_vld) begin
+                 a_reg[0] <= a;
+                 b_reg[0] <= b;
+             end
+             
+             for (int i = 1; i < 8; i++) 
+                 a_reg[i] <= a_reg[i-1];
+             
+             for (int i = 1; i < 4; i++) 
+                 b_reg[i] <= b_reg[i-1];
+         end
+     end
+     
+     isqrt #(.n_pipe_stages(4)) isqrt_c_inst (
+         .clk    (clk),
+         .rst    (rst),
+         .x_vld  (arg_vld),
+         .x      (c),
+         .y_vld  (sqrt_c_vld),
+         .y      (sqrt_c_res)
+     );
+     
+     isqrt #(.n_pipe_stages(4)) isqrt_bc_inst (
+         .clk    (clk),
+         .rst    (rst),
+         .x_vld  (sqrt_c_vld),
+         .x      (b_reg[3] + {16'b0, sqrt_c_res}),
+         .y_vld  (sqrt_bc_vld),
+         .y      (sqrt_bc_res)
+     );
+     
+     isqrt #(.n_pipe_stages(4)) isqrt_abc_inst (
+         .clk    (clk),
+         .rst    (rst),
+         .x_vld  (sqrt_bc_vld),
+         .x      (a_reg[7] + {16'b0, sqrt_bc_res}),
+         .y_vld  (res_vld),
+         .y      (res[15:0])
+     );
+     
+     assign res[31:16] = 16'b0;
 
 endmodule
